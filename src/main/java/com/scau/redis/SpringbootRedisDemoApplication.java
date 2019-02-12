d2be1298af861460f6482e43b3a024070f68b665
chen <chen1581961@163.com>
2019-02-12 15:54:46 +0800

    springboot整合redis

---
+++ src/main/java/com/scau/redis/SpringbootRedisDemoApplication.java
@@ -0,0 +1,14 @@
+package com.scau.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRedisDemoApplication.class, args);
+    }
+
+}
+
